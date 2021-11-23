02b2f09817093297885a17f5bbd2f857149fcb09
stephanwer <stephanwer>
2021-11-23 21:27:07 +0000

    first commit

---
+++ jobs/model_scoring_job.py
@@ -0,0 +1,26 @@
+# Databricks notebook source
+# MAGIC %run  ./../utils/config
+
+# COMMAND ----------
+
+# MAGIC %run ./../data/data_transformations
+
+# COMMAND ----------
+
+# TODD: load right env for model registry param
+
+model_name = "ml-model-demo"
+env = 'dev'
+experiment_id = env_experiment_id_dict['dev']
+
+client = mlflow.tracking.MlflowClient()
+production_model = client.get_latest_versions(name = model_name, stages = ["Production"])[0]
+DataProvider = LendingClubDataProvider(spark)
+
+# COMMAND ----------
+
+# TODO: change the scoring data to sth else
+
+_, X_test, _, _ = DataProvider.run()
+
+df_predictions = model.predict(X_test)
---
+++ test/unittest_model.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+from sklearn.metrics import roc_auc_score
+
+# COMMAND ----------
+
+# DBTITLE 1,Train New Model on Pull Request
+# MAGIC %run ./../jobs/model_trainning_job
+
+# COMMAND ----------
+
+# DBTITLE 1,Test if model fulfils quality requirements on curated data
+runs = mlflow.search_runs(experiment_ids=experiment_id)
+latest_run_index = runs.start_time.idxmax()
+latest_run = runs.loc[0]
+artifact_uri = latest_run['artifact_uri']
+
+model = mlflow.sklearn.load_model(artifact_uri + '/model')
+
+# COMMAND ----------
+
+X, Y = get_dummy_test_data()
+
+predictions = model.predict(X)
+
+roc = roc_auc_score(Y, predictions)
+
+assert roc > 0.8
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC test2
